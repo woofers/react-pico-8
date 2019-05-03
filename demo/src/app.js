ba0794390a4d8e61a41c1f3b3e0c6aa69fb6633a
Jaxson Van Doorn <woofers1tt@gmail.com>
2019-05-02 23:57:30 -0700

    Remove margin from demo

--- demo/src/app.js
+++ demo/src/app.js
@@ -5,7 +5,7 @@ import Pico8 from 'react-pico-8'
 const App = () => {
   return (
     <div style={{ background: '#222' }}>
-      <div style={{ marginLeft: '500px'}}>
+      <div>
         <Pico8 autoPlay={true} src="index.js" />
       </div>
     </div>
