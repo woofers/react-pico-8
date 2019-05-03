de6c0f406bad868cff3141bf3479ef9137d488fe
Jaxson Van Doorn <woofers1tt@gmail.com>
2019-05-02 23:44:16 -0700

    Make demo fullscreen

--- demo/src/app.js
+++ demo/src/app.js
@@ -3,11 +3,18 @@ import { jsx, css } from '@emotion/core'
 import Pico8 from 'react-pico-8'
 
 const App = () => {
+  const full = css`
+    background: #222;
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    opacity: 1;
+  `
   return (
-    <div style={{ background: '#222' }}>
-      <div style={{ marginLeft: '500px'}}>
-        <Pico8 autoPlay={true} src="index.js" />
-      </div>
+    <div css={full}>
+      <Pico8 autoPlay={true} src="index.js" legacyButtons={false} />
     </div>
   )
 }
