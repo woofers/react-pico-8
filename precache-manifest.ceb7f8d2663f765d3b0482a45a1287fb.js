7be615bf5751952e35206209e14264af81890a78
root <woofers1tt@gmail.com>
2019-05-05 19:45:20 +0000

    Updates

---
+++ precache-manifest.ceb7f8d2663f765d3b0482a45a1287fb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c2392d9b3f2af5b2e1a",
+    "url": "/react-pico-8/static/js/main.39117223.chunk.js"
+  },
+  {
+    "revision": "57070ef85fec624df4b6",
+    "url": "/react-pico-8/static/js/runtime~main.7269bc60.js"
+  },
+  {
+    "revision": "ea7b65407b63fc9288c4",
+    "url": "/react-pico-8/static/js/2.5b338320.chunk.js"
+  },
+  {
+    "revision": "ad6060adc128841ad0c4352f5e48557b",
+    "url": "/react-pico-8/index.html"
+  }
+];
