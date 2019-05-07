ac106363dd434a8a270e45cb9ad45c86a51f2e37
root <woofers1tt@gmail.com>
2019-05-07 22:34:13 +0000

    Updates

---
+++ precache-manifest.4ce448c09f49cfd05e5716c3ae062cc5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "6937677153dfb6693092",
+    "url": "/react-pico-8/static/js/main.b7789467.chunk.js"
+  },
+  {
+    "revision": "57070ef85fec624df4b6",
+    "url": "/react-pico-8/static/js/runtime~main.7269bc60.js"
+  },
+  {
+    "revision": "351fe978beb4f9a727d7",
+    "url": "/react-pico-8/static/js/2.d8c05b17.chunk.js"
+  },
+  {
+    "revision": "fa33ac144f074d1afe2634fe883dc21e",
+    "url": "/react-pico-8/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-pico-8/precache-manifest.48feed798bf5dab21619a514f944f309.js"
+  "/react-pico-8/precache-manifest.4ce448c09f49cfd05e5716c3ae062cc5.js"
 );
 
 workbox.clientsClaim();
