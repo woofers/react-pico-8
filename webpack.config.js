e0e57c1d16ac2a7040c8f4063629b37308f8ff94
Jaxson Van Doorn <woofers1tt@gmail.com>
2019-07-02 23:14:10 -0700

    Include React as a peer dependency

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,12 @@ const json = require('./package.json')
 const getPath = path => path.substring(0, path.lastIndexOf('/'))
 const getFile = path => path.substring(path.lastIndexOf('/') + 1)
 const mode = process.env.NODE_ENV
+const PeerDepsExternalsPlugin = require('peer-deps-externals-webpack-plugin')
 
 module.exports = {
+  plugins: [
+    new PeerDepsExternalsPlugin(),
+  ],
   entry: {
     'react-pico-8': `./${json.src}`,
     buttons: './src/external-buttons.js'
